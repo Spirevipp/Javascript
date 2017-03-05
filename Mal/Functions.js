5bb766084b158bbe6efcdb1c3d323624bc5de90b
Spirevipp <bjornar.helgeland@hotmail.com>
2017-03-05 19:12:51 +0100

    final import

---
+++ Mal/Functions.js
@@ -0,0 +1,35 @@
+function mousePressed() {
+
+}
+
+function mouseReleased() {
+
+}
+
+function mouseClicked() {
+
+}
+
+function mouseMoved() {
+
+}
+
+function mouseDragged() {
+
+}
+
+function mouseWheel() {
+
+}
+
+function keyPressed() {
+
+}
+
+function keyReleased() {
+
+}
+
+function keyTyped() {
+
+}
---
+++ Mal/Setup.js
@@ -0,0 +1,7 @@
+function preload() {
+
+}
+
+function setup(){
+  canvas = createCanvas(400, 400);
+}
