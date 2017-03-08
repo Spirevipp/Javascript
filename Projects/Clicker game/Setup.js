aa7ef5a6103805a32e69f7bf26056430e4f03dca
Spirevipp <bjornar.helgeland@hotmail.com>
2017-03-08 13:38:09 +0100

    add dom

--- Projects/Clicker game/Setup.js
+++ Projects/Clicker game/Setup.js
@@ -1,7 +1,7 @@
 function preload() {
   clickAreaImg = loadImage("assets/donate.png");  // 92x26
   coinImg = loadImage("assets/coin.png");         // 48x48
-  upgradeImg = loadImage("assets/guns.png");      // 224x224
+  upgradeImg = createImg("assets/guns.png");      // 224x224
   scareImg = loadImage("assets/scare.jpg");       // 164x237
   ben10 = loadImage("assets/ben10.png");
   reeeeGIF = createImg("assets/reee.gif");
@@ -11,4 +11,7 @@ function preload() {
 function setup() {
   reeeeGIF.hide();
   canvas = createCanvas(windowWidth-16, windowHeight-16);
+  uSize = min(width, height) / 12;
+  canvas.size(width-uSize, height);
+  upgradeImg.mouseClicked(upgrade);
 }
